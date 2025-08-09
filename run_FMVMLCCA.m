f24b7f1fc2f27f67144dcd271dfe0432e08a7b38
Asha Rani <asharani97@hotmail.com>
2025-08-09 22:44:32 +0530

    Update run_FMVMLCCA.m
    Updated norm function name

--- run_FMVMLCCA.m
+++ run_FMVMLCCA.m
@@ -13,8 +13,8 @@ else
 end
 Xte1 = I_x_te{1,idx1}; Xte2 = I_x_te{1,idx2};
 Zte1 = I_z_te{1,idx1}; Zte2 = I_z_te{1,idx2};
-A = MyNorm_new(I_x_te{1, idx1}, I_x_tr{1,idx1});
-B = MyNorm_new(I_x_te{1,idx2},I_x_tr{1,idx2});
+A = MyNorm_test(I_x_te{1, idx1}, I_x_tr{1,idx1});
+B = MyNorm_test(I_x_te{1,idx2},I_x_tr{1,idx2});
 
 case "mir"
 run create_mir.m;   ld = 30; mods = 2;
@@ -23,8 +23,8 @@ idx2 = input('Enter second index for modality: 1 : IMAGE, 2:ENG');
 Cx = {M_x_tr{1,idx1}, M_x_tr{1,idx2}};  Cz = {M_z_tr, M_z_tr};
 Xte1 = M_x_te{1,idx1}; Xte2 = M_x_te{1,idx2};
 Zte1 = M_z_te; Zte2 = M_z_te;
-A = MyNorm_new(M_x_te{1, idx1}, M_x_tr{1,idx1});
-B = MyNorm_new(M_x_te{1,idx2}, M_x_tr{1,idx2});
+A = MyNorm_test(M_x_te{1, idx1}, M_x_tr{1,idx1});
+B = MyNorm_test(M_x_te{1,idx2}, M_x_tr{1,idx2});
 
 case "mscoco"
 run create_coco.m;  ld = 60; mods = 2;
@@ -33,8 +33,8 @@ idx2 = input('Enter second index for modality: 1 : IMAGE, 2:ENG');
 Cx = {C_x_tr{1,idx1}, C_x_tr{1,idx2}};  Cz = {C_z_tr{1,idx1}, C_z_tr{1,idx2}};
 Xte1 = C_x_te{1,idx1}; Xte2 = C_x_te{1,idx2};
 Zte1 = C_z_te{1,idx1}; Zte2 = C_z_te{1,idx2};
-A = MyNorm_new(C_x_te{1, idx1}, C_x_tr{1,idx1});
-B = MyNorm_new(C_x_te{1,idx2}, C_x_tr{1,idx2});
+A = MyNorm_test(C_x_te{1, idx1}, C_x_tr{1,idx1});
+B = MyNorm_test(C_x_te{1,idx2}, C_x_tr{1,idx2});
 
 otherwise
 error('Unsupported dataset.');
@@ -52,6 +52,7 @@ else
     p_each =[2048;300;300;300];
     add_coco_ret_all(Wx, D, p_each, idx1, idx2,A, B, Zte1, Zte2,ld ,1,k);% d.X_te{1,index1}, d.X_te{1,index2}
 end
+
 
 
 
